2a707560a4524a4b6ec4e9b17dd061241edbe27c
John Le Drew <john@rainbowlaces.com>
2024-03-12 10:20:13 +0000

    add asyncEach

--- src/utils/async.ts
+++ src/utils/async.ts
@@ -2,3 +2,11 @@ export async function delay(timeout: number = 0) {
   if (!timeout) return new Promise((resolve) => setImmediate(resolve));
   return new Promise((resolve) => setTimeout(resolve, timeout));
 }
+
+export async function asyncEach<T>(
+  arr: Iterable<T> | Promise<Iterable<T>>,
+  fn: (item: T) => Promise<void>,
+) {
+  if (arr instanceof Promise) arr = await arr;
+  return Promise.all(Array.from(arr).map(async (item: T) => await fn(item)));
+}
