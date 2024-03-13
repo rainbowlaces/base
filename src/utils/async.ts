d078ae01303fc8cb2d62ba951df72e117bb8d604
John Le Drew <john@rainbowlaces.com>
2024-03-13 19:05:13 +0000

    added asyncMap

--- src/utils/async.ts
+++ src/utils/async.ts
@@ -2,11 +2,10 @@ export async function delay(timeout: number = 0) {
   if (!timeout) return new Promise((resolve) => setImmediate(resolve));
   return new Promise((resolve) => setTimeout(resolve, timeout));
 }
-
-export async function asyncEach<T>(
+export async function asyncMap<T, R = void>(
   arr: Iterable<T> | Promise<Iterable<T>>,
-  fn: (item: T) => Promise<void>,
-) {
+  fn: (item: T) => Promise<R>,
+): Promise<R[]> {
   if (arr instanceof Promise) arr = await arr;
-  return Promise.all(Array.from(arr).map(async (item: T) => await fn(item)));
+  return Promise.all(Array.from(arr).map((item: T) => fn(item)));
 }
