7618f14c1392d72067b993da51f9b2f5cd796530
John Le Drew <john@rainbowlaces.com>
2024-03-14 23:26:27 +0000

    Refactor asyncMap function to support async iterable

--- src/utils/async.ts
+++ src/utils/async.ts
@@ -2,10 +2,28 @@ export async function delay(timeout: number = 0) {
   if (!timeout) return new Promise((resolve) => setImmediate(resolve));
   return new Promise((resolve) => setTimeout(resolve, timeout));
 }
+
 export async function asyncMap<T, R = void>(
-  arr: Iterable<T> | Promise<Iterable<T>>,
+  iterable:
+    | Iterable<T>
+    | AsyncIterable<T>
+    | Promise<Iterable<T> | AsyncIterable<T>>,
   fn: (item: T) => Promise<R>,
 ): Promise<R[]> {
-  if (arr instanceof Promise) arr = await arr;
-  return Promise.all(Array.from(arr).map((item: T) => fn(item)));
+  // If the iterable is a promise, await it
+  iterable = await iterable;
+
+  // Check if the iterable is an async iterable
+  if (Symbol.asyncIterator in Object(iterable)) {
+    const asyncIterable = iterable as AsyncIterable<T>;
+    const results: R[] = [];
+    for await (const item of asyncIterable) {
+      results.push(await fn(item));
+    }
+    return results;
+  } else {
+    // The iterable is a synchronous iterable
+    const syncIterable = iterable as Iterable<T>;
+    return Promise.all(Array.from(syncIterable).map(fn));
+  }
 }
