984b9cfaf47fa96c605e75a7d396fa421b0cc30f
John Le Drew <john@rainbowlaces.com>
2025-08-16 13:17:24 +0100

    fix: Ensure proper teardown of WebSocket server and remove all listeners in BaseRequestHandler

--- src/core/requestHandler/baseRequestHandler.ts
+++ src/core/requestHandler/baseRequestHandler.ts
@@ -48,6 +48,9 @@ export class BaseRequestHandler {
   }
 
   async teardown() {
+    this.#server.removeAllListeners();
+    this.#wss.removeAllListeners();
+    this.#wss.close();
     this.#server.closeAllConnections();
   }
 
