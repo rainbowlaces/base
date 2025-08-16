1fced866b36d066a9d04be3a11af8939bbb23b24
John Le Drew <john@rainbowlaces.com>
2025-08-17 00:47:25 +0100

    chore: add comment to clarify purpose of cli.js as a proxy for base init

--- packages/create-base/cli.js
+++ packages/create-base/cli.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
 import { spawn } from 'child_process';
+
+// Simple proxy: pass all args straight through to base init.
 const args = ['@rainbowlaces/base', 'init', ...process.argv.slice(2)];
 spawn('npx', args, { stdio: 'inherit' });
